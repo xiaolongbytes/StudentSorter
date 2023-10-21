9768b96d1206884e687a5da8d04df6806ea5ad4f
ytraiba <yasintraiba@gmail.com>
2023-10-21 12:40:53 -0400

    Seating Generator
    User can now create a seating chart based on the number of columns entered

--- src/components/SeatBuilder/index.tsx
+++ src/components/SeatBuilder/index.tsx
@@ -1,74 +1,158 @@
 import RosterTable from '@/common/components/ClassRoster';
+import useStudents from '../../hooks/useStudents'
 import { StudentData } from '@/common/components/Student';
 import Button from 'react-bootstrap/Button';
 import Col from 'react-bootstrap/Col';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import SeatsUserInput from './SeatUserInput';
+import Form from 'react-bootstrap/Form';
+import { useState, useEffect } from 'react';
+import Table from 'react-bootstrap/Table';
+import './style.module.css'
 
-// Fake data for rendering, delete when real data is ready
-export const students: StudentData[] = [
-    {
-        id: 0,
-        first: 'John',
-        last: 'Smith',
-        groupID: 1,
-    },
-    {
-        id: 1,
-        first: 'John',
-        last: 'Doe',
-        groupID: 1,
-    },
-    {
-        id: 2,
-        first: 'Jane',
-        last: 'Smith',
-        groupID: 1,
-    },
-    {
-        id: 3,
-        first: 'John',
-        last: 'Jingle Heimershmidt',
-        groupID: 1,
-    },
-    {
-        id: 4,
-        first: 'April',
-        last: 'Wang',
-        groupID: 1,
-    },
-];
+// // Fake data for rendering, delete when real data is ready
+// export const students: StudentData[] = [
+//     {
+//         id: 0,
+//         first: 'John',
+//         last: 'Smith',
+//         groupID: 1,
+//     },
+//     {
+//         id: 1,
+//         first: 'John',
+//         last: 'Doe',
+//         groupID: 1,
+//     },
+//     {
+//         id: 2,
+//         first: 'Jane',
+//         last: 'Smith',
+//         groupID: 1,
+//     },
+//     {
+//         id: 3,
+//         first: 'John',
+//         last: 'Jingle Heimershmidt',
+//         groupID: 1,
+//     },
+//     {
+//         id: 4,
+//         first: 'April',
+//         last: 'Wang',
+//         groupID: 1,
+//     },
+//     {
+//         id: 5,
+//         first: 'Jane',
+//         last: 'Smith',
+//         groupID: 1,
+//     },
+//     {
+//         id: 6,
+//         first: 'John',
+//         last: 'Jingle Heimershmidt',
+//         groupID: 1,
+//     },
+//     {
+//         id: 7,
+//         first: 'April',
+//         last: 'Wang',
+//         groupID: 1,
+//     },
+//     {
+//         id: 8,
+//         first: 'Jane',
+//         last: 'Smith',
+//         groupID: 1,
+//     },
+//     {
+//         id: 9,
+//         first: 'John',
+//         last: 'Jingle Heimershmidt',
+//         groupID: 1,
+//     },
+//     {
+//         id: 10,
+//         first: 'April',
+//         last: 'Wang',
+//         groupID: 1,
+//     },
+// ];
 
-const SeatBuilder = () => {
+
+const SeatBuilder: React.FC = () => {
+    const [numColumns, setNumColumns] = useState<number>(0);
+    const [seatingChart, setSeatingChart] = useState<StudentData[][]>([]);
+    const { students, addStudent, deleteStudent, handleFileUpload, processCSVUpload } = useStudents();
+  
+    const generateSeatingChart = () => {
+      const newSeatingChart: StudentData[][] = [];
+  
+      students.forEach((student, index) => {
+        const row = Math.floor(index / numColumns); 
+        if (!newSeatingChart[row]) {
+          newSeatingChart[row] = [];
+        }
+        newSeatingChart[row].push(student);
+      });
+  
+      setSeatingChart(newSeatingChart);
+    };
+  
+    useEffect(() => {
+      generateSeatingChart();
+    }, [numColumns, students]);
+  
     return (
-        <Container>
-            <Row>
-                <Col md="auto">
-                    <h1>Class Roster</h1>
-                </Col>
-                <Col md="auto">
-                    <Button variant="primary">Import Roster</Button>
-                </Col>
-            </Row>
-            <Row>
-                <Col>
-                    <RosterTable students={students} />
-                </Col>
-                <Col>
-                    <SeatsUserInput />
-                </Col>
-            </Row>
-            <Row>
-                <Col md="auto">
-                    <h1>Seating Chart</h1>
-                </Col>
-                <Col md="auto">
-                    <Button variant="primary">Export Seating Chart</Button>
-                </Col>
-            </Row>
-        </Container>
+      <Container>
+        <Row>
+          <Col md="auto">
+            <h1>Class List</h1>
+          </Col>
+          <Form>
+            <Form.Group>
+              <Form.Label></Form.Label>
+              <Form.Control type="file" accept=".csv" onChange={handleFileUpload} />
+            </Form.Group>
+            <Button variant="primary" onClick={(e) => processCSVUpload(e)}>Upload CSV</Button>
+          </Form>
+        </Row>
+        <Row>
+          <Col>
+            {/* Assuming RosterTable is a valid component */}
+            <RosterTable students={students} deleteStudent={deleteStudent} addStudent={addStudent} />
+          </Col>
+          <Col>
+            {/* Assuming SeatsUserInput is a valid component */}
+            <SeatsUserInput setNumColumns={setNumColumns} />
+          </Col>
+        </Row>
+        <Row>
+          <Col md="auto">
+            <h1>Seating Chart</h1>
+          </Col>
+          <Col md="auto">
+            <Button variant="primary">Export Seating Chart</Button>
+          </Col>
+        </Row>
+        {/* Display the seating chart here */}
+        <Table striped bordered hover size="sm">
+          <tbody>
+            {seatingChart.map((row, rowIndex) => (
+              <tr key={rowIndex}>
+                {row.map((student) => (
+                  <td key={student.id}>
+                    {`${student.first} ${student.last}`}
+                  </td>
+                ))}
+              </tr>
+            ))}
+          </tbody>
+        </Table>
+      </Container>
     );
-};
-
-export default SeatBuilder;
+  };
+  
+  export default SeatBuilder;
